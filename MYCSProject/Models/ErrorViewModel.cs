472c530dc0699bc58720a43ebb9419c0bc30c05a
amir-mohammad-aghapour <seyedamirmohammadaghapour1378@gmail.com>
2022-11-23 11:54:50 -0800

    First Commit

---
+++ MYCSProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MYCSProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
